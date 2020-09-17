1317a8797b6a801349ca7dd99589c8a9e186b565
shreyaspkl <69814100+shreyaspkl@users.noreply.github.com>
2020-09-17 18:55:27 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,17 @@
+import pickle
+with open("model_pickle2","rb") as f:
+    mp=pickle.load(f)
+def u_in():
+  text = st.text_input("Enter your review: ")
+  a = []
+  a.append(text)
+  return a
 
+st.title("ML Project")
+st.subheader("This is sentiment analysis model made by Shreyas Mandaokar")
+df = u_in()
+pred = mp.predict(df) 
+if pred == 0:
+  st.write("Negative Sentiment")
+else:
+  st.write("Positive Sentiment")
